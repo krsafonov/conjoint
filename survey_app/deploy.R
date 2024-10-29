e0233624f83980728b3a3e2faf9000a27d338d06
Milana Arsenteva <136199989+milarsenteva@users.noreply.github.com>
2024-10-29 21:35:11 +0300

    Add files via upload

--- survey_app/deploy.R
+++ survey_app/deploy.R
@@ -1,10 +1,10 @@
 library("rsconnect")
 library(shiny)
 
-setwd("~/Documents/ma/conjoint/survey_app/")
+setwd("C:/Users/mila1/OneDrive/Рабочий стол/диплом/experiment_design/survey_app/")
 
 runApp()
 
 deployApp()
-# deployApp('../post_survey_app/')
+
 
