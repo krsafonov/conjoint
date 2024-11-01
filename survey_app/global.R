6f46bdc12f1e49a7e4d27580c4481aa59fc95378
Milana Arsenteva <136199989+milarsenteva@users.noreply.github.com>
2024-11-02 01:11:07 +0300

    Add files via upload

--- survey_app/global.R
+++ survey_app/global.R
@@ -1,12 +1,12 @@
 library("idefix")
 
 al <- list(
-  c("70k", "110k", "150k", "200k"), # wage
-  c("10 мин", "30 мин", "60 мин"), # commute time
+  c("100k", "140k", "180k", "230k"), # wage
+  c("30 мин", "60 мин", "90 мин"), # commute time
   c("Full-time", "Гибрид", "Полностью удаленный"), # work mode
   c("Формальная", "Полуформальная", "Неформальная"), # corporate culture
-  c("Современный", "Классический", "Неблагоустроенный / устаревший"), # Office
-  c("Через 1 год", "Через 3 года", "Через 5 лет"), # promotion criterion
+  c("Премиальный", "Современный", "Традиционный"), # Office
+  c("Через полгода", "Через 1 год", "Через 2 года"), # promotion criterion
   c("< 25 лет", "25 - 35 лет", "35 - 45 лет", '> 45 лет'), # average age of the team
   c("20%", "50%", "70%") # task
 )
@@ -19,7 +19,7 @@ mu <- c(70, # no.choice (constant utility)
         -10, -20, # Office
         -10, -20, # promotion criterion
         +5, -5, -10, # average age of the team
-        0, -10 # task
+        -15, -30 # task
 )
 
 v <- diag(length(mu[c(-1, -2)])) # Prior variance.
@@ -32,7 +32,7 @@ ps <- list(no.choice.pd, pd)
 coded <- c("C", "C", "D", "D", "D", "D", "D", "D")
 alt <- c(0, 0, 0, 1)
 
-exp <- readRDS('june_design.RData')
+exp <- readRDS('October_design.RData')
 
 des <- exp$design
 
@@ -43,13 +43,12 @@ attributes_eng <- c('wage', 'commute_time', 'work_mode', 'corporate_culture',
                 'office', 'promotion', 'team_age', 'task')
 
 labels_eng <- vector(mode = "list", length(attributes))
-labels_eng[[1]] <- c("70k", "110k", "150k", "200k") # wage
-labels_eng[[2]] <- c("10 min", "30 min", "60 min") # commute time
-# labels_eng[[3]] <- c("State-owned", "Private", "Non-commerical") # sectors
+labels_eng[[1]] <- c("100k", "140k", "180k", "230k") # wage
+labels_eng[[2]] <- c("30 min", "60 min", "90 min") # commute time
 labels_eng[[3]] <- c("Full-time", "Hybrid", "Online") # work mode
 labels_eng[[4]] <- c("Formal", "Intermediate", "Informal") # corporate culture
-labels_eng[[5]] <- c("Good", "Intermediate", "Bad") # Office
-labels_eng[[6]] <- c("yes", "no") # promotion criterion
+labels_eng[[5]] <- c("Premium", "Modern", "Classic") # Office
+labels_eng[[6]] <- c("In 6 months", "In 1 year", "In 2 years") # promotion criterion
 labels_eng[[7]] <- c("< 25", "25 - 35", "35 - 45", '> 45') # average age of the team
 labels_eng[[8]] <- c("20%", "50%", "70%") # task
 
@@ -58,17 +57,16 @@ attributes_rus <- c('Зарплата', 'Время в пути', 'График 
                     'Офис', 'Первое повышение', 'Средний возраст команды', 'Доля неитересных задач')
 
 labels_rus <- vector(mode = "list", length(attributes))
-labels_rus[[1]] <- c("70k", "110k", "150k", "200k") # wage
-labels_rus[[2]] <- c("10 мин", "30 мин", "60 мин") # commute time
+labels_rus[[1]] <- c("100k", "140k", "180k", "230k") # wage
+labels_rus[[2]] <- c("30 мин", "60 мин", "90 мин") # commute time
 labels_rus[[3]] <- c("Full-time", "Гибрид", "Полностью удаленный") # work mode
 labels_rus[[4]] <- c("Формальная", "Полуформальная", "Неформальная") # corporate culture
-labels_rus[[5]] <- c("Современный", "Классический", "Неблагоустроенный / устаревший") # Office
-labels_rus[[6]] <- c("Через 1 год", "Через 3 года", "Через 5 лет") # promotion criterion
+labels_rus[[5]] <- c("Премиальный", "Современный", "Традиционный") # Office
+labels_rus[[6]] <- c("Через полгода", "Через 1 год", "Через 2 года") # promotion criterion
 labels_rus[[7]] <- c("< 25 лет", "25 - 35 лет", "35 - 45 лет", '> 45 лет') # average age of the team
 labels_rus[[8]] <- c("20%", "50%", "70%") # task
 
 code <- coded
-
 # Russian lang selected
 alternatives <- alternatives_rus
 labels <- labels_rus
